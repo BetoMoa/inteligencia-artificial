b9d03357170b540ffa05c04afaff4302c29c9216
BetoMoa <emora3@ucol.mx>
2020-10-07 13:46:21 -0500

    rey_enrique_7mo

---
+++ Erik Alberto Mora Alvarez.pl
@@ -0,0 +1,34 @@
+es_padre(enrique7, enrique8).
+es_padre(enrique7, arturo).
+es_padre(enrique7, margarita).
+es_padre(enrique8, maria).
+es_padre(enrique8, isabel).
+es_padre(enrique8, eduardo).
+es_madre(catalina, maria).
+es_madre(ana, isabel).
+es_madre(juana, eduardo).
+es_abuelo(enrique7, maria).
+es_abuelo(enrique7, isabel).
+es_abuelo(enrique7, eduardo).
+es_hermano(enrique8, arturo).
+es_hermano(enrique8, margarita).
+es_hermano(arturo, enrique8).
+es_hermano(arturo, margarita).
+es_hermano(eduardo, maria).
+es_hermano(eduardo, isabel).
+es_hermana(margarita, enrique8).
+es_hermana(margarita, arturo).
+es_hermana(maria, isabel).
+es_hermana(maria, eduardo).
+es_hermana(isabel, maria).
+es_hermana(isabel, eduardo).
+es_hombre(enrique7).
+es_hombre(enrique8).
+es_hombre(arturo).
+es_hombre(eduardo).
+es_mujer(margarita).
+es_mujer(catalina).
+es_mujer(ana).
+es_mujer(juana).
+es_mujer(maria).
+es_mujer(isabel).
